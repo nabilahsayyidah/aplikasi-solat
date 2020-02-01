c538ea51438ef67bcc7603a5f9bb5766844af67f
Putri Huriati <huriatiputri@gmail.com>
2020-02-01 14:54:52 +0700

    hjd

--- app/src/main/java/com/example/aplikasi_sholat/api/ApiService.java
+++ app/src/main/java/com/example/aplikasi_sholat/api/ApiService.java
@@ -2,6 +2,7 @@ package com.example.aplikasi_sholat.api;
 
 import com.example.aplikasi_sholat.model.Item;
 import com.example.aplikasi_sholat.model.Modul;
+import com.example.aplikasi_sholat.model.ResponseItem;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
